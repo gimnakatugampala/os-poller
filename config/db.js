69db18eb311aaada3a18512657003b4b69549dc7
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-05-31 18:49:32 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 // Mongoose connect
-mongoose.connect('mongodb+srv://Gimna:9922557gimna@nodetuts.qlhc3.mongodb.net/pusherpoll?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://<username>:<password>@nodetuts.qlhc3.mongodb.net/pusherpoll?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
 .then(() => console.log('MongoDb Connected'))
 .catch(err => console.log(err))
